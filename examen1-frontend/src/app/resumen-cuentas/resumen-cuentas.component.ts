891f13eb09096175bc13544b727654f184c85af7
JuanJC199 <juancarranza96@outlook.com>
2021-05-18 22:12:07 -0600

    falta resumen

--- examen1-frontend/src/app/resumen-cuentas/resumen-cuentas.component.ts
+++ examen1-frontend/src/app/resumen-cuentas/resumen-cuentas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountMocks } from '../shared/mocks/AccountMocks';
 import { TransactionMock } from '../shared/mocks/TransactionMocks';
 import { Accounts } from '../shared/models/Accounts';
 import { Transactions } from '../shared/models/Transactions';
@@ -14,19 +15,23 @@ export class ResumenCuentasComponent implements OnInit {
   total: number = 0;
   items !: Transactions[];
   accounts !: Accounts[];
+  ratio : number = 0;
   constructor() { }
 
   ngOnInit(): void {
     this.items = TransactionMock;
+    this.accounts = AccountMocks;
     this.Llenador();
   }
 
   Llenador() {
     this.items.forEach((item) => {
+      var proratio = this.accounts?.find(p => p.Id== item.AccountId);
+      this.ratio != proratio?.ConversionRate;
       if(item.Amount >0)
-         this.income = this.income +(item.Amount * this.accounts?.findIndex(p => p.Id== item.AccountId));
+         this.income = this.income +(item.Amount * this.ratio );
       else
-        this.expenses = this.expenses + (item.Amount * this.accounts?.findIndex(p => p.Id== item.AccountId));
+        this.expenses = this.expenses + (item.Amount * this.ratio);
     })
     this.total = this.income + this.expenses;
   }

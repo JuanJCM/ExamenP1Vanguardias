31611ba626ba4b77a26b9a09e9bd0c7050dac644
JuanJC199 <juancarranza96@outlook.com>
2021-05-18 21:59:38 -0600

    loco pero funciona

---
+++ examen1-frontend/src/app/shared/mocks/AccountMocks.ts
@@ -0,0 +1,7 @@
+import {Accounts} from "../models/Accounts";
+
+export const AccountMocks : Array<Accounts> =[
+    {Amount:200,ConversionRate:25.00,Currency:"HNL",Id:1,Name:"johnnie"},
+    {Amount:300,ConversionRate:1.00,Currency:"USD",Id:2,Name:"johnnie"},
+    {Amount:400,ConversionRate:30.00,Currency:"EUR",Id:3,Name:"johnnie"}
+];
--- examen1-frontend/src/app/transacciones/transacciones.component.ts
+++ examen1-frontend/src/app/transacciones/transacciones.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Accounts } from '../shared/models/Accounts';
 import { Transactions } from '../shared/models/Transactions';
+import {AccountMocks} from '../shared/mocks/AccountMocks';
+import { TransactionMock } from '../shared/mocks/TransactionMocks';
 
 @Component({
   selector: 'app-transacciones',
@@ -8,16 +10,22 @@ import { Transactions } from '../shared/models/Transactions';
   styleUrls: ['./transacciones.component.css']
 })
 export class TransaccionesComponent implements OnInit {
-  items !: Transactions;
+  items !: Transactions[];
   accounts!: Accounts[];
   cuenta !: string;
   constructor() { }
 
   ngOnInit(): void {
+    this.accounts = AccountMocks;
+    this.items= TransactionMock;
   }
   NombreCuenta(Accountid: number){
     var account = this.accounts?.find(p=> p.Id == Accountid);
     return account?.Name;
+  }
+  CurrencyState(Accountid: number){
+    var account = this.accounts?.find(p=> p.Id == Accountid);
+    return account?.Currency;
   }
 
 }
